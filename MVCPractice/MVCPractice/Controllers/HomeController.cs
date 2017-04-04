9579ad8da17d5d52868409cf061105e18643c2f0
Zubair Ali <Zubair Ali>
2017-04-04 23:48:15 +0500

    you commit message

---
+++ MVCPractice/MVCPractice/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult signup()
+        {
+            return View();
+        }
+
+	}
+}
